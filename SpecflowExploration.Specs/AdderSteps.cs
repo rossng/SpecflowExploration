32ac28af1afde818cba7063b1e58d63d5b350b46
Ross Gardiner <Ross.Gardiner@3shape.com>
2019-11-20 15:27:12 +0100

    Add initial SpecFlow solution

---
+++ SpecflowExploration.Specs/AdderSteps.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+using TechTalk.SpecFlow;
+
+namespace SpecflowExploration.Specs
+{
+    [Binding]
+    public class AdderSteps
+    {
+        private readonly Adder _adder = new Adder();
+        private List<int> _operands = new List<int>();
+
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int p0)
+        {
+            _adder.Operands.Add(p0);
+            _operands.Add(p0);
+        }
+
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+            _adder.Calculate();
+        }
+
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int p0)
+        {
+            Assert.AreEqual(p0, _operands.Sum());
+        }
+    }
+}
---
+++ SpecflowExploration/Adder.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace SpecflowExploration
+{
+    public class Adder
+    {
+        public List<int> Operands { get; set; } = new List<int>();
+        public int Result { get; private set; }
+
+        public void Calculate()
+        {
+            Result = Operands.Sum();
+        }
+    }
+}
